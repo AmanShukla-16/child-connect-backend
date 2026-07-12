cc5652f79cb88bd5eda62418320e25639d49be09
Aman Shukla <amanmshukla16@gmail.com>
2026-07-13 04:06:37 +0530

    Use MYSQL_DATABASE

--- db.py
+++ db.py
@@ -1,17 +1,11 @@
 import os
 import mysql.connector
 
-print("MYSQLHOST =", os.getenv("MYSQLHOST"))
-print("MYSQLUSER =", os.getenv("MYSQLUSER"))
-print("MYSQLPASSWORD =", os.getenv("MYSQLPASSWORD"))
-print("MYSQLDATABASE =", os.getenv("MYSQLDATABASE"))
-print("MYSQLPORT =", os.getenv("MYSQLPORT"))
-
 db = mysql.connector.connect(
     host=os.getenv("MYSQLHOST"),
     user=os.getenv("MYSQLUSER"),
     password=os.getenv("MYSQLPASSWORD"),
-    database=os.getenv("MYSQLDATABASE"),
+    database=os.getenv("MYSQL_DATABASE"),   # <-- change this
     port=int(os.getenv("MYSQLPORT")),
 )
 
