f82877d3fcd1489edcdb099f558c55113018f7ad
Aman Shukla <amanmshukla16@gmail.com>
2026-07-13 03:55:03 +0530

    Debug Railway environment variables

--- db.py
+++ db.py
@@ -1,6 +1,12 @@
 import os
 import mysql.connector
 
+print("MYSQLHOST =", os.getenv("MYSQLHOST"))
+print("MYSQLUSER =", os.getenv("MYSQLUSER"))
+print("MYSQLPASSWORD =", os.getenv("MYSQLPASSWORD"))
+print("MYSQLDATABASE =", os.getenv("MYSQLDATABASE"))
+print("MYSQLPORT =", os.getenv("MYSQLPORT"))
+
 db = mysql.connector.connect(
     host=os.getenv("MYSQLHOST"),
     user=os.getenv("MYSQLUSER"),
